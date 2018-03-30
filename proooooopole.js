175c9c70b0c47310498a79de18a836b02b4964b7
haryhorde07 <haryhorde07@gmail.com>
2018-03-31 04:02:56 +0700

    Add files via upload

---
+++ proooooopole.js
@@ -0,0 +1,26 @@
+<div expr:id='&quot;ads1&quot; + data:post.id'/>
+<div style='clear:both; margin:10px 0'>
+IKLAN
+</div>
+<div expr:id='&quot;ads2&quot; + data:post.id'><data:post.body/></div>
+<script type='text/javascript'>
+var obj0=document.getElementById(&quot;ads1<data:post.id/>&quot;);
+var obj1=document.getElementById(&quot;ads2<data:post.id/>&quot;);
+var s=obj1.innerHTML;
+var t=s.substr(0,s.length/2);
+var r=t.lastIndexOf(&quot; &quot;);
+if(r&gt;0) {obj0.innerHTML=s.substr(0,r);obj1.innerHTML=s.substr(r+1);}
+</script>
+
+&lt;div style=&quot;display:block;text-align:center;margin:0 auto;&quot;&gt;
+&lt;script async src=&quot;//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js&quot;&gt;&lt;/script&gt;
+&lt;ins class=&quot;adsbygoogle&quot;
+     style=&quot;display:block; text-align:center;&quot;
+     data-ad-format=&quot;fluid&quot;
+     data-ad-layout=&quot;in-article&quot;
+     data-ad-client=&quot;ca-pub-9119734088927911&quot;
+     data-ad-slot=&quot;3753065228&quot;&gt;&lt;/ins&gt;
+&lt;script&gt;
+     (adsbygoogle = window.adsbygoogle || []).push({});
+&lt;/script&gt;
+&lt;/div&gt;
